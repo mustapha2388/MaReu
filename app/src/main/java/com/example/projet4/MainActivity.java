7bed864b5376c7e59bbf9b261c07f804600948a7
mouss <mouss2388@gmail.com>
2023-09-28 15:37:11 +0200

    Enable ViewBinding

--- app/src/main/java/com/example/projet4/MainActivity.java
+++ app/src/main/java/com/example/projet4/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.projet4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.projet4.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mBinding.getRoot();
+        setContentView(view);
     }
 }
