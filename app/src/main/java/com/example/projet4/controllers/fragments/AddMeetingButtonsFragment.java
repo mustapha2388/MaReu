3885aa2380a99722208bf62ee2dd91c610efb298
mouss <mouss2388@gmail.com>
2023-10-16 22:16:44 +0200

    Fix bug which clear list rooms after rotate
    + Get  savedInstanceState and update room adapter

--- app/src/main/java/com/example/projet4/controllers/fragments/AddMeetingButtonsFragment.java
+++ app/src/main/java/com/example/projet4/controllers/fragments/AddMeetingButtonsFragment.java
@@ -10,12 +10,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.projet4.R;
 import com.example.projet4.databinding.FragAddMeetingButtonsBinding;
 import com.example.projet4.models.Meeting;
 import com.google.android.material.timepicker.MaterialTimePicker;
@@ -33,6 +36,7 @@ public class AddMeetingButtonsFragment extends Fragment {
     public FragAddMeetingButtonsBinding bindingButtonsFrag;
     private DataPassListener dataPassListener;
     private Meeting mMeeting;
+    private ArrayAdapter<String> adapter;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -74,21 +78,60 @@ public class AddMeetingButtonsFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         bindingButtonsFrag = FragAddMeetingButtonsBinding.inflate(inflater, container, false);
+        setAdapterRoom(savedInstanceState);
 
         // Inflate the layout for this fragment
         return bindingButtonsFrag.getRoot();
     }
 
+    private void setAdapterRoom(Bundle savedInstanceState) {
+
+        if (savedInstanceState != null) {
+            String[] adapterData = savedInstanceState.getStringArray("adapterData");
+            if (adapterData != null) {
+                adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_dropdown_item_1line, adapterData);
+                bindingButtonsFrag.roomSelected.setAdapter(adapter);
+            }
+        }
+        if (adapter == null) {
+            initRoomsAdapter();
+        }
+    }
+
+    private void initRoomsAdapter() {
+        // Set up the adapter and populate items
+        String[] initialData = getResources().getStringArray(R.array.my_string_array);
+        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_dropdown_item_1line, initialData);
+        bindingButtonsFrag.roomSelected.setAdapter(adapter);
+
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mMeeting = new Meeting();
+
         initColorForPicker();
         setupListeners();
+
+
     }
 
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (adapter != null) {
+            int count = adapter.getCount();
+            String[] items = new String[count];
+            for (int i = 0; i < count; i++) {
+                items[i] = adapter.getItem(i);
+            }
+            outState.putStringArray("adapterData", items);
+        }
+    }
 
     private void initColorForPicker() {
         int color = -96704;
