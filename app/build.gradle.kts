4211abbb6694c55b34982fda344a9c8778b14101
mouss <mouss2388@gmail.com>
2023-10-29 13:11:50 +0100

    update Gson Library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:2.19.0")
-    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("com.google.code.gson:gson:2.10.1")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
