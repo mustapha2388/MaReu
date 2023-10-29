109800694568431d166c1bdd8a5b3ccf22d7ff0c
mouss <mouss2388@gmail.com>
2023-10-29 14:36:29 +0100

    clean unit testing code

--- app/src/main/java/com/example/projet4/controllers/ui/MainActivity.java
+++ app/src/main/java/com/example/projet4/controllers/ui/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
